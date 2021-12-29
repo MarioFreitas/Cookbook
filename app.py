0f25b8d7a01cb4406ccb10a7585e7403aa538555
MarioRaul <raulfreitas@hotmail.com>
2021-12-29 11:58:43 -0500

    start on boot

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from app_index import *
 from app_recipe import *
 from app_edit import *
 from app_new import *
+import os
 
 if __name__ == '__main__':
+    try:
+        os.chdir('/home/pi/shared/Cookbook')
+    except FileNotFoundError:
+        pass
     app.run(debug=True, host='0.0.0.0')
